9c0595b21b109c1c4ccb847f87700b63e91f2d77
Brian Tu <feihcsim824@gmail.com>
2017-12-09 12:15:16 -0500

    readme and test cleanup

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # dat-palindrome-doe
 determine if given string is a palindrome or not
+
+configure the algorithm with custom options. the default configuration is this (merged with custom configs when provided)
+```js
+const defaultConfig = {
+
+  // first deal with special cases (assessed before validation of input)
+  specialIs: value => value === 0 || value === '',
+  specialIsNot: value => !value || value === true,
+
+  // then validate input
+  validateInput: value => typeof value === "string" || typeof value === "number",
+
+  // algorithm functions
+  filter: char => /^[a-z0-9]+$/i.test(char), // isAlphaNumeric
+  compare: (a, b) => !!a && !!b && a.toLowerCase() === b.toLowerCase(), // leftEqualsRightWhenBothLowercase
+  
+}
+```
--- tests/default.test.js
+++ tests/default.test.js
@@ -4,144 +4,74 @@ describe("isPalindrome with default settings", () => {
 
   const isPalindrome = datPalindromeDoe()
 
-  test('"race car" is a palindrome', () => {
-    expect(isPalindrome("race car")).toBeTruthy()
-  })
+  test('"race car" is a palindrome', () => expect(isPalindrome("race car")).toBeTruthy())
 
-  test('"not a isPalindrome" is not a palindrome', () => {
-    expect(isPalindrome("not a isPalindrome")).toBeFalsy()
-  })
+  test('"not a isPalindrome" is not a palindrome', () => expect(isPalindrome("not a isPalindrome")).toBeFalsy())
 
-  test('"A man, a plan, a canal. Panama" is a palindrome', () => {
-    expect(isPalindrome("A man, a plan, a canal. Panama")).toBeTruthy()
-  })
+  test('"A man, a plan, a canal. Panama" is a palindrome', () => expect(isPalindrome("A man, a plan, a canal. Panama")).toBeTruthy())
 
-  test('"never odd or even" is a palindrome', () => {
-    expect(isPalindrome("never odd or even")).toBeTruthy()
-  })
+  test('"never odd or even" is a palindrome', () => expect(isPalindrome("never odd or even")).toBeTruthy())
 
-  test('"never odd or even" is a palindrome', () => {
-    expect(isPalindrome("never odd or even")).toBeTruthy()
-  })
+  test('"never odd or even" is a palindrome', () => expect(isPalindrome("never odd or even")).toBeTruthy())
 
-  test('"nope" is not a palindrome', () => {
-    expect(isPalindrome("nope")).toBeFalsy()
-  })
+  test('"nope" is not a palindrome', () => expect(isPalindrome("nope")).toBeFalsy())
 
-  test('"almostomla" is not a palindrome', () => {
-    expect(isPalindrome("almostomla")).toBeFalsy()
-  })
+  test('"almostomla" is not a palindrome', () => expect(isPalindrome("almostomla")).toBeFalsy())
 
-  test('"My age is 0, 0 si ega ym." is not a palindrome', () => {
-    expect(isPalindrome("My age is 0, 0 si ega ym.")).toBeTruthy()
-  })
+  test('"My age is 0, 0 si ega ym." is not a palindrome', () => expect(isPalindrome("My age is 0, 0 si ega ym.")).toBeTruthy())
 
-  test('"1 eye for of 1 eye." is not a palindrome', () => {
-    expect(isPalindrome("1 eye for of 1 eye.")).toBeFalsy()
-  })
+  test('"1 eye for of 1 eye." is not a palindrome', () => expect(isPalindrome("1 eye for of 1 eye.")).toBeFalsy())
 
-  test('"0_0 (: /-\ :) 0–0" is not a palindrome', () => {
-    expect(isPalindrome("0_0 (: /-\ :) 0–0")).toBeTruthy()
-  })
+  test('"0_0 (: /-\ :) 0–0" is not a palindrome', () => expect(isPalindrome("0_0 (: /-\ :) 0–0")).toBeTruthy())
 
-  test('"________bob" is not a palindrome', () => {
-    expect(isPalindrome("________bob")).toBeTruthy()
-  })
+  test('"________bob" is not a palindrome', () => expect(isPalindrome("________bob")).toBeTruthy())
 
-  test('"b________ob" is not a palindrome', () => {
-    expect(isPalindrome("b________ob")).toBeTruthy()
-  })
+  test('"b________ob" is not a palindrome', () => expect(isPalindrome("b________ob")).toBeTruthy())
 
-  test('"jared" is not a palindrome', () => {
-    expect(isPalindrome('jared')).toBeFalsy()
-  })
+  test('"jared" is not a palindrome', () => expect(isPalindrome('jared')).toBeFalsy())
 
-  test('an empty string is a palindrome', () => {
-    expect(isPalindrome('')).toBeTruthy()
-  })
+  test('an empty string is a palindrome', () => expect(isPalindrome('')).toBeTruthy())
 
-  test('a single character is a palindrome', () => {
-    expect(isPalindrome('a')).toBeTruthy()
-  })
+  test('a single character is a palindrome', () => expect(isPalindrome('a')).toBeTruthy())
 
-  test('two identical characters are a palindrome', () => {
-    expect(isPalindrome('aa')).toBeTruthy()
-  })
+  test('two identical characters are a palindrome', () => expect(isPalindrome('aa')).toBeTruthy())
   
-  test('two differing characters are not a palindrome', () => {
-    expect(isPalindrome('ab')).toBeFalsy()
-  })
+  test('two differing characters are not a palindrome', () => expect(isPalindrome('ab')).toBeFalsy())
 
-  test('"racecar" is a palindrome', () => {
-    expect(isPalindrome('racecar')).toBeTruthy()
-  })
+  test('"racecar" is a palindrome', () => expect(isPalindrome('racecar')).toBeTruthy())
   
-  test('"Amore Roma" is a palindrome', () => {
-    expect(isPalindrome('Amore Roma')).toBeTruthy()
-  })
+  test('"Amore Roma" is a palindrome', () => expect(isPalindrome('Amore Roma')).toBeTruthy())
   
-  test('"A man, a plan, a canal: Panama" is a palindrome', () => {
-    expect(isPalindrome('A man, a plan, a canal: Panama')).toBeTruthy()
-  })
+  test('"A man, a plan, a canal: Panama" is a palindrome', () => expect(isPalindrome('A man, a plan, a canal: Panama')).toBeTruthy())
   
-  test("\"No 'x' in 'Nixon'\" is a palindrome", () => {
-    expect(isPalindrome("No 'x' in 'Nixon'")).toBeTruthy()
-  })
+  test("\"No 'x' in 'Nixon'\" is a palindrome", () => expect(isPalindrome("No 'x' in 'Nixon'")).toBeTruthy())
   
-  test('42 is not a palindrome', () => {
-    expect(isPalindrome(42)).toBeFalsy()
-  })
+  test('42 is not a palindrome', () => expect(isPalindrome(42)).toBeFalsy())
   
-  test('1 is a palindrome', () => {
-    expect(isPalindrome(1)).toBeTruthy()
-  })
+  test('1 is a palindrome', () => expect(isPalindrome(1)).toBeTruthy())
   
-  test('11 is a palindrome', () => {
-    expect(isPalindrome(11)).toBeTruthy()
-  })
+  test('11 is a palindrome', () => expect(isPalindrome(11)).toBeTruthy())
   
-  test('121 is a palindrome', () => {
-    expect(isPalindrome(121)).toBeTruthy()
-  })
+  test('121 is a palindrome', () => expect(isPalindrome(121)).toBeTruthy())
   
-  test('0 is a palindrome', () => {
-    expect(isPalindrome(0)).toBeTruthy()
-  })
+  test('0 is a palindrome', () => expect(isPalindrome(0)).toBeTruthy())
   
-  test('1e10 + 1 is a palindrome', () => {
-    expect(isPalindrome(1e10 + 1)).toBeTruthy()
-  })
+  test('1e10 + 1 is a palindrome', () => expect(isPalindrome(1e10 + 1)).toBeTruthy())
   
-  test('true is not a palindrome', () => {
-    expect(isPalindrome(true)).toBeFalsy()
-  })
+  test('true is not a palindrome', () => expect(isPalindrome(true)).toBeFalsy())
   
-  test('false is not a palindrome', () => {
-    expect(isPalindrome(false)).toBeFalsy()
-  })
+  test('false is not a palindrome', () => expect(isPalindrome(false)).toBeFalsy())
   
-  test('null is not a palindrome', () => {
-    expect(isPalindrome(null)).toBeFalsy()
-  })
+  test('null is not a palindrome', () => expect(isPalindrome(null)).toBeFalsy())
   
-  test('undefined is not a palindrome', () => {
-    expect(isPalindrome(undefined)).toBeFalsy()
-  })
+  test('undefined is not a palindrome', () => expect(isPalindrome(undefined)).toBeFalsy())
   
-  test('NaN is not a palindrome', () => {
-    expect(isPalindrome(NaN)).toBeFalsy()
-  })
+  test('NaN is not a palindrome', () => expect(isPalindrome(NaN)).toBeFalsy())
   
-  test('-1 is not a palindrome', () => {
-    expect(isPalindrome(-1)).toBeTruthy()
-  })
+  test('-1 is not a palindrome', () => expect(isPalindrome(-1)).toBeTruthy())
   
-  test('"!@#$" is not a palindrome', () => {
-    expect(isPalindrome('!@#$', false)).toBeFalsy()
-  })
+  test('"!@#$" is not a palindrome', () => expect(isPalindrome('!@#$', false)).toBeFalsy())
   
-  test('"!@#$#@!" is a palindrome', () => {
-    expect(isPalindrome('!@#$#@!', false)).toBeFalsy()
-  })
+  test('"!@#$#@!" is a palindrome', () => expect(isPalindrome('!@#$#@!', false)).toBeFalsy())
   
 })
