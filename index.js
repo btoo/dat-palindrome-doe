3e219836ec5ecb330f2dc488095687a2f8571949
Brian Tu <Brian.Tu@michaelkors.com>
2017-12-09 02:53:21 -0500

    more tests and extensibility

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
-const isAlphaNumeric = char => /^[a-z0-9]+$/i.test(char)
-    , leftEqualsRightWhenBothLowercase = s => (a, b) => s[a].toLowerCase() === s[b].toLowerCase()
+const defaultConfig = {
 
-module.exports = (filter = isAlphaNumeric, comparator = leftEqualsRightWhenBothLowercase) => s => {
+  // first deal with special cases
+  specialIs: value => value === 0 || value === '',
+  specialIsNot: value => !value || value === true,
+
+  // then validate input
+  validateInput: value => typeof value === "string" || typeof value === "number",
+
+  // algorithm functions
+  filter: char => /^[a-z0-9]+$/i.test(char), // isAlphaNumeric
+  comparator: value => (a, b) => value[a] && value[b] && value[a].toLowerCase() === value[b].toLowerCase(), // leftEqualsRightWhenBothLowercase
   
-  const filter = isAlphaNumeric
-      , compare = comparator(s)
-
-  let left = 0
-    , right = s.length - 1
-  
-  do {
-    
-    while(!filter(s[left])) left++
-    
-    while(!filter(s[right])) right--
-    
-    if(!compare(left, right)) return false
-    
-    left++
-    right--
-
-  } while(left < right)
-
-  return true
 
 }
 
-const isPalindrome = module.exports()
+module.exports = config => {
+
+  const {
+    validateInput,
+    specialIs,
+    specialIsNot,
+    filter,
+    comparator,
+  } = { ...defaultConfig, ...config }
+
+  return v => {
+    
+    if(specialIs(v)) return true
+    if(specialIsNot(v) || !validateInput(v)) return false
+
+    const value = v.toString()
+        , compare = comparator(value)
+
+    let left = 0
+      , right = value.length - 1
+    
+    while(left < right) {
+      
+      while(!filter(value[left])) left++
+      while(!filter(value[right])) right--
+
+      if(!compare(left, right)) return false
+      
+      left++
+      right--
+
+    }
+
+    return true
+  }
+}
---
+++ tests/custom.test.js
@@ -0,0 +1,101 @@
+const datPalindromeDoe = require('../index')
+
+describe("isPalindrome with custom filter and comparator functions", () => {
+  
+  const isPalindrome = datPalindromeDoe({
+    filter: char => /[\w!@#\$%\^\&*\)\(+=._-]/i.test(char)
+  })
+
+  test('"jared" is not a palindrome', () => {
+    expect(isPalindrome('jared')).toBeFalsy()
+  })
+
+  test('an empty string is a palindrome', () => {
+    expect(isPalindrome('')).toBeTruthy()
+  })
+  
+  test('a single character is a palindrome', () => {
+    expect(isPalindrome('a')).toBeTruthy()
+  })
+
+  test('two identical characters are a palindrome', () => {
+    expect(isPalindrome('aa')).toBeTruthy()
+  })
+  
+  test('two differing characters are not a palindrome', () => {
+    expect(isPalindrome('ab')).toBeFalsy()
+  })
+
+  test('"racecar" is a palindrome', () => {
+    expect(isPalindrome('racecar')).toBeTruthy()
+  })
+  
+  test('"Amore Roma" is a palindrome', () => {
+    expect(isPalindrome('Amore Roma')).toBeTruthy()
+  })
+  
+  test('"A man, a plan, a canal: Panama" is a palindrome', () => {
+    expect(isPalindrome('A man, a plan, a canal: Panama')).toBeTruthy()
+  })
+  
+  test("\"No 'x' in 'Nixon'\" is a palindrome", () => {
+    expect(isPalindrome("No 'x' in 'Nixon'")).toBeTruthy()
+  })
+  
+  test('42 is not a palindrome', () => {
+    expect(isPalindrome(42)).toBeFalsy()
+  })
+  
+  test('1 is a palindrome', () => {
+    expect(isPalindrome(1)).toBeTruthy()
+  })
+  
+  test('11 is a palindrome', () => {
+    expect(isPalindrome(11)).toBeTruthy()
+  })
+  
+  test('121 is a palindrome', () => {
+    expect(isPalindrome(121)).toBeTruthy()
+  })
+  
+  test('0 is a palindrome', () => {
+    expect(isPalindrome(0)).toBeTruthy()
+  })
+  
+  test('1e10 + 1 is a palindrome', () => {
+    expect(isPalindrome(1e10 + 1)).toBeTruthy()
+  })
+  
+  test('true is not a palindrome', () => {
+    expect(isPalindrome(true)).toBeFalsy()
+  })
+  
+  test('false is not a palindrome', () => {
+    expect(isPalindrome(false)).toBeFalsy()
+  })
+  
+  test('null is not a palindrome', () => {
+    expect(isPalindrome(null)).toBeFalsy()
+  })
+  
+  test('undefined is not a palindrome', () => {
+    expect(isPalindrome(undefined)).toBeFalsy()
+  })
+  
+  test('NaN is not a palindrome', () => {
+    expect(isPalindrome(NaN)).toBeFalsy()
+  })
+  
+  test('-1 is not a palindrome', () => {
+    expect(isPalindrome(-1)).toBeFalsy()
+  })
+  
+  test('"!@#$" is not a palindrome', () => {
+    expect(isPalindrome('!@#$', false)).toBeFalsy()
+  })
+  
+  test('"!@#$#@!" is a palindrome', () => {
+    expect(isPalindrome('!@#$#@!', false)).toBeTruthy()
+  })
+
+})
